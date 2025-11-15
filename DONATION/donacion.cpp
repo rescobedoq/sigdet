640d1bcb41a727edfb85e06d9762f55a069d7f81
roni <rcompanocca@unsa.edu.pe>
2025-11-14 21:25:55 -0500

    comenzando nuestro proyecto

---
+++ DONATION/donacion.cpp
@@ -0,0 +1,14 @@
+#include "donacion.h"
+#include "ui_donacion.h"
+
+Donacion::Donacion(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Donacion)
+{
+    ui->setupUi(this);
+}
+
+Donacion::~Donacion()
+{
+    delete ui;
+}
---
+++ DONATION/donacion.h
@@ -0,0 +1,23 @@
+#ifndef DONACION_H
+#define DONACION_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Donacion;
+}
+QT_END_NAMESPACE
+
+class Donacion : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Donacion(QWidget *parent = nullptr);
+    ~Donacion();
+
+private:
+    Ui::Donacion *ui;
+};
+#endif // DONACION_H
